ab7b4e08599bb30f3caeda6976998d667d4c80bf
dqdynamo <147186555+dqdynamo@users.noreply.github.com>
2023-11-20 20:36:54 +0600

    script.js

--- As4/script.js
+++ As4/script.js
@@ -13,8 +13,7 @@ document.getElementById('exploreButton').addEventListener('click', function () {
     var exploreButton = document.getElementById('exploreButton');
     exploreButton.classList.add('explode');
 
-    // Удалите класс explode после завершения анимации
     setTimeout(function () {
         exploreButton.classList.remove('explode');
-    }, 500); // 500ms соответствует времени анимации
+    }, 500); 
 });
